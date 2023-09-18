474e99be6cec22df9d4efbc7670a94e987cfce00
Brian-Weloba <bweloba@gmail.com>
2023-09-18 04:13:05 +0000

    UPDATE UI

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,3 +1,6 @@
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
-  }
+  return (
+    <div className="flex align-center justify-center font-jost">
+      <h1>404 - PAGE NOT FOUND</h1>
+    </div>)
+}
