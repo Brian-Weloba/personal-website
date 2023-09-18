b8a2e8103ff84212e638341f383c9a0c514d09fb
Brian-Weloba <bweloba@gmail.com>
2023-09-18 15:23:38 +0000

    t

--- pages/api/simulateError.tsx
+++ pages/api/simulateError.tsx
@@ -1,6 +1,7 @@
 // pages/api/simulateError.js
 export default (req: any, res: any) => {
     // Simulate a server-side error by throwing an error
-    throw new Error('This is a simulated server-side error.');
+    // Simulate a server-side error by returning a 500 status code
+  res.status(500).json({ error: 'Internal Server Error' });
   };
   
