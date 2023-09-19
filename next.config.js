a21a8d836cb48a328e99c80295315a2d54ea8a93
Brian-Weloba <bweloba@gmail.com>
2023-09-19 07:19:18 +0000

    PWA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('@imbios/next-pwa')({
-	dest: 'public',
+const withPWA = require("@imbios/next-pwa")({
+	dest: "public",
+	register: true, // Register the service worker during build time.
+	scope: "/", // Define the service worker's scope.
+	sw: "service-worker.js", // Specify the service worker file name.
 });
 
 const nextConfig = {
 	images: {
-		domains: ['images.unsplash.com', 'now-playing-sooty.vercel.app'],
+		domains: ["images.unsplash.com", "now-playing-sooty.vercel.app"],
 	},
 
 	// pwa: {
@@ -13,9 +16,9 @@ const nextConfig = {
 	// 	disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode.
 	// 	// Additional PWA configuration options can be added here.
 	// 	// For example:
-	// 	// register: true, // Register the service worker during build time.
-	// 	// scope: '/', // Define the service worker's scope.
-	// 	// sw: 'service-worker.js', // Specify the service worker file name.
+	// register: true, // Register the service worker during build time.
+	// scope: '/', // Define the service worker's scope.
+	// sw: 'service-worker.js', // Specify the service worker file name.
 	// 	// ...
 	// },
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,19 @@
 // pages/_document.js (or _document.tsx if using TypeScript)
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
 				<Head>
-					<link rel="manifest" href="../public/manifest.json" />
+					<link rel="manifest" href="/manifest.json" />
+					<link rel="apple-touch-icon" href="/icon-192x192.png" />
 					<link
-						rel="apple-touch-icon"
-						href="../public/icon.png"></link>
+						rel="shortcut icon"
+						type="image/x-icon"
+						href="/icon-32x32.png"
+					/>
+
 					<meta name="theme-color" content="#292423" />
 				</Head>
 				<body>
